e3fd2d69d818ac8be647dfab18dcda555991806d
Roberto Machado Lopes Ribeiro <robertomlr@icloud.com>
2020-08-28 19:57:06 -0300

    Teste Spring

---
+++ projeto-teste/src/main/java/com/petshop/projetoteste/ProjetoTesteApplication.java
@@ -0,0 +1,14 @@
+package com.petshop.projetoteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTesteApplication.class, args);
+		System.out.println("Olá mundo!");
+	}
+
+}
---
+++ projeto-teste/src/test/java/com/petshop/projetoteste/ProjetoTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshop.projetoteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
